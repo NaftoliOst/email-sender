30c0614f07071fd79a2437801aaf323333bb2230
Naftoli Ost <naftoliyost@gmail.com>
2021-01-06 01:42:17 +0200

    Add WPF UI
    Adds basic WPF UI

---
+++ EmailSenderUI/MailSender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net.Mail;
+
+namespace EmailSenderUI
+{
+    class MailSender
+    {
+        public static void Send(string to, string subject, string body, string password)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+
+                mailMessage.From = new MailAddress("literallyallothernamesaretaken@gmail.com", "Custom name");
+                mailMessage.To.Add(to);
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new System.Net.NetworkCredential("literallyallothernamesaretaken@gmail.com", password);
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mailMessage);
+                MessageBox.Show("Mail Send");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ EmailSenderUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace EmailSenderUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            string to = To.Text;
+            string subjet = Subject.Text;
+            string body = Body.Text;
+            string password = Password.Password;
+            MailSender.Send(to, subjet, body, password);
+            
+
+        }
+
+        
+    }
+}
